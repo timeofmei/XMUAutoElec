4a8182c8c528ed011983d32cca09ed04fe6c299a
timeofmei <timeofmei@gmail.com>
2021-03-30 12:43:50 +0800

    a little optimize

--- parse.py
+++ parse.py
@@ -20,6 +20,9 @@ class Elec:
             "dxdateEnd_DDD_C_FNPWS": "0:0:-1:-10000:-10000:0:0px:-10000:1"
         }
 
+    def find(pattern, string, start=0, end=None, format=str):
+        return format(re.compile(pattern).search(string).group()[start:end])
+
     def getElec(self):
         self.getInitPage()
         self.getSquaredPage()
@@ -56,10 +59,8 @@ class Elec:
         self.doc.append(etree.HTML(self.resp[2].text))
         try:
             finalResult = self.doc[2].xpath('//*[@id="lableft"]/text()')[0]
-            df = float(re.compile(
-                "账户余额：.*元").search(finalResult).group()[5:-1])
-            du = float(re.compile(
-                "剩余电量：.*度").search(finalResult).group()[5:-1])
+            df = self.find("账户余额：.*元", finalResult, 5, -1, float)
+            du = self.find("剩余电量：.*度", finalResult, 5, -1, float)
             result = {
                 "账户余额": df,
                 "剩余电量": du
