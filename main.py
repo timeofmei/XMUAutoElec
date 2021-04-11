492527533cd70bde6e66b8f791b30c19e9d4a81e
timeofmei <timeofmei@outlook.com>
2021-04-11 14:53:58 +0800

    delete comments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
+import ujson
+import httpx
 from flask import Flask, request
 from parse import Elec
 app = Flask(__name__)
@@ -7,12 +8,16 @@ app = Flask(__name__)
 @app.route('/', methods=["POST"])
 def hello_world():
     data = request.get_data()
-    json_data = json.loads(data.decode("utf-8"))
+    json_data = ujson.loads(data.decode("utf-8"))
     xiaoqu = json_data.get("xiaoqu")
     louming = json_data.get("louming")
     fangjian = json_data.get("fangjian")
     elec = Elec(xiaoqu, louming, fangjian)
-    return elec.getElec()
+    try:
+        result = ujson.dumps(elec.getElec(), ensure_ascii=False)
+    except httpx.ConnectTimeout:
+        return ujson.dumps({"账户余额": -2.0, "剩余电量": -2.0})
+    return result
 
 
 if __name__ == "__main__":
